b9a9e6501a7a021237ab530828ff0da8353b6f90
Hittugit <friendlyguy54@gmail.com>
2019-05-08 18:10:56 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void AndroidApps() {
+		System.out.println("AppiumTest");
+		
+	}
+@Test
+public void IOSApps() {
+	System.out.println("IOSApps");
+	
+}
+}
